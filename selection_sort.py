df383eda2995fd9eb0ae7da2876a5db24bc90d64
DeniseIra02 <irajubilo@gmail.com>
2023-01-23 22:53:37 +0800

    Def funtion for the method of selection sort. Assigning a variable to hold min position.

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,11 @@
 #Selection Sort
 
+#logic
+def select_sort (array_nums):
+    for i in range(9):
+        #assigning i to hold min position
+        min_pos = i
+        
+
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
