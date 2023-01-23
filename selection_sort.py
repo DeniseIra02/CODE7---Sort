b47995403565a6324defc277edfb86df8199b518
DeniseIra02 <irajubilo@gmail.com>
2023-01-23 22:59:43 +0800

    Added another loop for recucing the size of the unsorted array.

--- selection_sort.py
+++ selection_sort.py
@@ -6,6 +6,12 @@ def select_sort (array_nums):
         #assigning i to hold min position
         min_pos = i
         
+        for j in range(i, 10):
+            #change the min position 
+            #check 
+            if array_nums[j] < array_nums[min_pos]:
+            
+        
 
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
