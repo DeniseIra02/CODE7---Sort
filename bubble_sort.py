b5bd6bb2eb843c6266de314611f4b6b4b1f1af00
DeniseIra02 <irajubilo@gmail.com>
2023-01-23 23:20:31 +0800

    For loop for the outer and another for loop for the inner.

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,11 @@
 
 #logic
 def bub_sort(array_nums):
-    
+    #outer loop
+    for i in range(len(array_nums)- 1, 0, -1):
+        #inner loop
+        for j in range(i):
+        
 
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
