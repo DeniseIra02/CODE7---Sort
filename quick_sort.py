e8e96de98eb1d6334a7d4b5a6a4cf870c29f287a
DeniseIra02 <irajubilo@gmail.com>
2023-01-25 00:14:16 +0800

    Another funtion which is the partition, this area will get the same parameters.

--- quick_sort.py
+++ quick_sort.py
@@ -4,6 +4,7 @@
 def quick_sort(array_nums, left, right):
     #check
     if left < right:
+        partition_position = partition(array_nums, left, right)
 
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
