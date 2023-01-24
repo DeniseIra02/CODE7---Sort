0f3a39ba66e9d2396bf847597c5c934e624b87c0
DeniseIra02 <irajubilo@gmail.com>
2023-01-24 23:27:12 +0800

    Add the recursion condition. after having the sub arrays, it will also do the algo of merge sort in both arrays.

--- merge_sort.py
+++ merge_sort.py
@@ -4,10 +4,14 @@
 def merge_sort(array_nums):
     #check
     if len(array_nums) > 1: 
-        #sub array - from beggining to the middle point
+        #sub array - from beginning to the middle point
         left_arr_nums = array_nums[:len(array_nums)//2]
         #sub array - from the middle point to the end
         right_arr_nums = array_nums[len(array_nums)//2:]
+        
+    #recursion
+    merge_sort(left_arr_nums)
+    merge_sort(right_arr_nums)
         
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
