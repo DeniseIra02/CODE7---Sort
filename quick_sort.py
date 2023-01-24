ef18e13047bd07dd3974f5f26de998fc4d904c43
DeniseIra02 <irajubilo@gmail.com>
2023-01-25 00:21:19 +0800

    calling quick sort function for the position of elements that is greater than or less than the pivot.

--- quick_sort.py
+++ quick_sort.py
@@ -5,6 +5,13 @@ def quick_sort(array_nums, left, right):
     #check
     if left < right:
         partition_position = partition(array_nums, left, right)
+        #all elements that are less than the pivot element
+        quick_sort(array_nums, left, partition_position - 1)
+        #all elements that are greater then the pivot element
+        quick_sort(array_nums, partition_position + 1, right)
+        
+
+def partition(array_nums, left, right)
 
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
