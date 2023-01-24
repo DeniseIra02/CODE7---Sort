30a6e7e087b66e3ca3276ab89e6bc21b68a638ca
DeniseIra02 <irajubilo@gmail.com>
2023-01-24 23:24:41 +0800

    Def function method for the algo of merge sort. If statement to check if the length of the array is greater than 1, if it so, divide it into 2 sub arrays which are left - from the beginning to the middle point and right - from the middle point to the end.

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,13 @@
 #Merge Sort
 
+#Logic
+def merge_sort(array_nums):
+    #check
+    if len(array_nums) > 1: 
+        #sub array - from beggining to the middle point
+        left_arr_nums = array_nums[:len(array_nums)//2]
+        #sub array - from the middle point to the end
+        right_arr_nums = array_nums[len(array_nums)//2:]
+        
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
