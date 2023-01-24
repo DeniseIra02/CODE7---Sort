48fa2b43613b82b0b4358ebb9e35df67503c7e99
DeniseIra02 <irajubilo@gmail.com>
2023-01-25 00:32:13 +0800

    then afterwards, check if i and j crossed to each other and if not, implement the swap condition.

--- quick_sort.py
+++ quick_sort.py
@@ -25,5 +25,14 @@ def partition(array_nums, left, right):
         while j > left and array_nums[j] >= pivot:
             j -= 1
         
+        #check
+        if i < j:
+            #swap condition
+            array_nums[i], array_nums[j] = array_nums[j], array_nums[i]
+            
+            
+            
+                        
+        
 #array
 array_nums = [100, 7, 49, 1, 70, 36, 39, 69, 92, 48]
